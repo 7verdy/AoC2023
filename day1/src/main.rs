7eae2ff7b783dc28a1db92407d85f7cda08274ce
Victor-Emmanuel PROVOST <victor-emmanuel.provost@epita.fr>
2023-12-02 00:28:12 +0100

    aoc2023(day1): add part 2.

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,6 +1,6 @@
 use std::fs::read_to_string;
 
-fn main() {
+fn first_part() {
     let mut sum_calibration_values = 0;
     for line in read_to_string("input.txt").unwrap().lines() {
         let first_digit = line
@@ -24,4 +24,48 @@ fn main() {
         sum_calibration_values += first_digit * 10 + last_digit;
     }
     println!("Result: {}", sum_calibration_values);
+}
+
+
+fn get_spelled_out_digit_or_value(s: &str, start_index: usize) -> (i32, usize) {
+    let (mut value, mut index) = (-1, start_index);
+    for i in index..s.chars().count() {
+        let splice = &s[i..];
+        if splice.starts_with("one")    { value = 1; }
+        if splice.starts_with("two")    { value = 2; }
+        if splice.starts_with("three")  { value = 3; }
+        if splice.starts_with("four")   { value = 4; }
+        if splice.starts_with("five")   { value = 5; }
+        if splice.starts_with("six")    { value = 6; }
+        if splice.starts_with("seven")  { value = 7; }
+        if splice.starts_with("eight")  { value = 8; }
+        if splice.starts_with("nine")   { value = 9; }
+        else if s.chars().nth(i).unwrap().is_digit(10) {
+            value = s.chars().nth(i).unwrap().to_digit(10).unwrap() as i32;
+        }
+        if value != -1 {
+            index = i;
+            if start_index == 0 { break; }
+        }
+    }
+    return (value, index);
+}
+
+fn second_part() {
+    let mut sum_calibration_values = 0;
+    for line in read_to_string("input.txt").unwrap().lines() {
+
+        let (first_digit, index) = get_spelled_out_digit_or_value(line, 0);
+        let (mut last_digit, _) = get_spelled_out_digit_or_value(line, index + 1);
+        if last_digit == -1 { last_digit = first_digit; }
+
+        sum_calibration_values += first_digit * 10 + last_digit;
+
+    }
+    println!("Result: {}", sum_calibration_values);
+}
+
+fn main() {
+    first_part();
+    second_part();
 }
