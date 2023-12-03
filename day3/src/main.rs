ac10870df01cc59b7bb13728ddb293aa2b5cc5f5
Victor-Emmanuel PROVOST <victor-emmanuel.provost@epita.fr>
2023-12-03 11:00:10 +0100

    aoc2023(day3): add part 2.

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead};
 
@@ -73,7 +74,7 @@ fn parse_schematic(s: Vec<String>) -> u32 {
 }
 
 fn first_part() {
-    let file = File::open("input.txt").expect("Error in reading file");
+    let file = File::open("example.txt").expect("Error in reading file");
     let reader = io::BufReader::new(file);
     let lines: io::Result<Vec<String>> = reader.lines().collect();
 
@@ -86,6 +87,107 @@ fn first_part() {
     }
 }
 
+
+fn get_adjacency(s: Vec<String>, number: String, i: usize, j: usize) -> (usize, usize) {
+    let digits = number.chars().count();
+    if i > 0 {
+        // Top left or top right
+        if j > 0 && s[i - 1].chars().nth(j - 1).unwrap() == '*' {
+            return (i - 1, j - 1);
+        }
+        if j < s[i].chars().count() - 1 - digits
+            && s[i - 1].chars().nth(j + digits).unwrap() == '*'
+        {
+            return (i - 1, j + digits);
+        }
+        // Top
+        for x in 0..digits {
+            if s[i - 1].chars().nth(j + x).unwrap() == '*' {
+                return (i - 1, j + x);
+            }
+        }
+    }
+    if i < s.len() - 1 {
+        // Bottom left or bottom right
+        if j > 0 && s[i + 1].chars().nth(j - 1).unwrap() == '*' {
+            return (i + 1, j - 1);
+        }
+        if j < s[i].chars().count() - 1 - digits
+            && s[i + 1].chars().nth(j + digits).unwrap() == '*'
+        {
+            return (i + 1, j + digits);
+        }
+        // Bottom
+        for x in 0..digits {
+            if s[i + 1].chars().nth(j + x).unwrap() == '*' {
+                return (i + 1, j + x);
+            }
+        }
+    }
+    // Left
+    if j > 0 {
+        if s[i].chars().nth(j - 1).unwrap() == '*' {
+            return (i, j - 1);
+        }
+    }
+    // Right
+    if j < s[i].chars().count() - 1 - digits {
+        if s[i].chars().nth(j + digits).unwrap() == '*' {
+            return (i, j + digits);
+        }
+    }
+
+    ( s.len(), s[i].chars().count() )
+}
+
+fn parse_part_2(s: Vec<String>) -> u32 {
+    let mut sum_valid_numbers = 0;
+    let mut gear_parts: HashMap<String, String> = HashMap::new();
+
+    for i in 0..s.len() {
+        let mut j = 0;
+        while j < s[i].chars().count() {
+            if !s[i].chars().nth(j).unwrap().is_digit(10) {
+                j += 1;
+                continue;
+            }
+            let number = s[i]
+                .chars()
+                .skip(j)
+                .take_while(|&c| c.is_digit(10))
+                .collect::<String>();
+            let (gear_y, gear_x) = get_adjacency(s.clone(), number.clone(), i, j);
+            if gear_y != s.len() {
+                let yow = gear_y.to_owned();
+                let xow = gear_x.to_owned();
+                let key = format!("{yow}-{xow}");
+                if gear_parts.contains_key(&key) {
+                    sum_valid_numbers += number.parse::<u32>().unwrap() * (*gear_parts.get(&key).unwrap()).parse::<u32>().unwrap();
+                } else {
+                    gear_parts.insert(key, number.clone());
+                }
+            }
+            j += number.chars().count();
+        }
+    }
+    sum_valid_numbers
+}
+
+
+fn second_part() {
+    let file = File::open("input.txt").expect("Error in reading file");
+    let reader = io::BufReader::new(file);
+    let lines: io::Result<Vec<String>> = reader.lines().collect();
+
+    match lines {
+        Ok(lines) => {
+            let res = parse_part_2(lines);
+            println!("The result is: {}", res);
+        }
+        Err(err) => eprintln!("Error reading files: {}", err),
+    }
+}
 fn main() {
     first_part();
+    second_part();
 }
