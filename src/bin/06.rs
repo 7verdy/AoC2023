95ac6b901c955f28ebbaa4cfbe8d5650aef874e0
Victor-Emmanuel PROVOST <victor-emmanuel.provost@epita.fr>
2023-12-12 17:19:17 +0100

    aoc2023(day6): refactor using a sequence of iterators to have a code more rust-like.

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,57 +1,89 @@
 use std::fs::read_to_string;
 
-fn first_part(filename: String) {
-    let data = read_to_string(filename).unwrap();
+use num::{PrimInt, Float};
 
-    let times = data
-        .lines().nth(0).unwrap().split(": ").nth(1).unwrap()
-        .split_whitespace().map(|x| x.parse::<u32>().unwrap())
-        .collect::<Vec<u32>>();
-    let distances = data
-        .lines().nth(1).unwrap().split(": ").nth(1).unwrap()
-        .split_whitespace().map(|x| x.parse::<u32>().unwrap())
-        .collect::<Vec<u32>>();
-
-    let mut total_ways = 1;
-
-    for (idx, time) in times.iter().enumerate() {
-        let mut local_ways = 0;
-        for x in 0..*time {
-            let distance = (time - x) * x;
-            if distance > distances[idx] {
-                local_ways += 1;
+fn first_part(filename: String) -> u64 {
+    read_to_string(filename)
+        .unwrap()
+        .split_once("\n")
+        .map(|(times, distances)| {
+            (
+                times
+                    .chars()
+                    .skip_while(|&c| c != ' ')
+                    .collect::<String>()
+                    .split_whitespace()
+                    .map(|s| s.parse::<u64>().unwrap().to_owned())
+                    .collect::<Vec<_>>(),
+                distances
+                    .chars()
+                    .skip_while(|&c| c != ' ')
+                    .collect::<String>()
+                    .split_whitespace()
+                    .map(|s| s.parse::<u64>().unwrap().to_owned())
+                    .collect::<Vec<_>>(),
+            )
+        })
+        .map(|(times, distances)| {
+            let mut total_ways = 1;
+            for (idx, time) in times.iter().enumerate() {
+                let mut local_ways = 0;
+                for x in 0..*time {
+                    let distance = (time - x) * x;
+                    if distance > distances[idx] {
+                        local_ways += 1;
+                    }
+                }
+                total_ways *= local_ways;
             }
-        }
-        total_ways *= local_ways;
-    }
-    println!("The result is: {}", total_ways);
+            total_ways
+        })
+        .unwrap()
 }
 
-fn second_part(filename: String) {
-    let data = read_to_string(filename).unwrap();
-    let time = data
-        .lines().nth(0).unwrap().split(": ").nth(1).unwrap()
-        .split_whitespace().collect::<String>()
-        .parse::<u64>()
-        .unwrap();
-    let distance = data
-        .lines().nth(1).unwrap().split(": ").nth(1).unwrap()
-        .split_whitespace().collect::<String>()
-        .parse::<u64>()
-        .unwrap();
-
-    let mut total_ways = 0;
-    for x in 0..time {
-        let local_distance: u64 = (time - x) * x;
-        if local_distance > distance {
-            total_ways += 1;
-        }
-    }
-    println!("The result is: {}", total_ways);
+fn second_part(filename: String) -> u64 {
+    read_to_string(filename)
+        .unwrap()
+        .split_once("\n")
+        .map(|(times, distances)| {
+            (
+                times
+                    .chars()
+                    .skip_while(|&c| c != ' ')
+                    .collect::<String>()
+                    .split_whitespace()
+                    .collect::<String>(),
+                distances
+                    .chars()
+                    .skip_while(|&c| c != ' ')
+                    .collect::<String>()
+                    .split_whitespace()
+                    .collect::<String>(),
+            )
+        })
+        .map(|(time, distance)| {
+            let time = time.parse::<u64>().unwrap();
+            let distance = distance.parse::<u64>().unwrap();
+            let mut total_ways = 0;
+            for x in 0..time {
+                let curr_distance = (time - x) * x;
+                if curr_distance > distance {
+                    total_ways += 1;
+                }
+            }
+            total_ways
+        })
+        .unwrap()
 }
 fn main() {
-    let cwd = format!("{}{}", std::env::current_dir().unwrap().display(), "/src/bin/");
+    let cwd = format!(
+        "{}{}",
+        std::env::current_dir().unwrap().display(),
+        "/src/bin/"
+    );
     let filename = format!("{}06_{}", cwd, "input.txt");
-    first_part(filename.clone());
-    second_part(filename.clone());
+    let p1_res = first_part(filename.clone());
+    println!("The result for step 1 is: {}", p1_res);
+    let p2_res = second_part(filename.clone());
+    println!("The result for step 2 is: {}", p2_res);
 }
